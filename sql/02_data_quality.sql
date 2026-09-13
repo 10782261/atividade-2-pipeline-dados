d86d7497783440c6d122928a833c85775b9ecced
10782261 <nettima0711@gmail.com>
2026-09-13 16:38:03 -0300

    Update 02_data_quality.sql

--- sql/02_data_quality.sql
+++ sql/02_data_quality.sql
@@ -1,1 +1,34 @@
+SELECT
+    p.pedido_id,
+    p.cliente_id,
+    p.product_id,
+    p.quantidade,
 
+    CONCAT_WS(
+        '; ',
+        CASE
+            WHEN p.quantidade <= 0
+            THEN 'quantidade <= 0'
+        END,
+        CASE
+            WHEN c.cliente_id IS NULL
+            THEN 'cliente_id inexistente'
+        END,
+        CASE
+            WHEN pr.product_id IS NULL
+            THEN 'product_id inexistente'
+        END
+    ) AS motivo_rejeicao
+
+FROM atividade2.pedidos p
+
+LEFT JOIN atividade2.clientes c
+    ON p.cliente_id = c.cliente_id
+
+LEFT JOIN atividade2.produtos pr
+    ON p.product_id = pr.product_id
+
+WHERE
+    p.quantidade <= 0
+    OR c.cliente_id IS NULL
+    OR pr.product_id IS NULL;
