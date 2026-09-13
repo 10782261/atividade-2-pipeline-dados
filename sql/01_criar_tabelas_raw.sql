c6f182456dd79698f9408ab63fa8fb780d4a530a
10782261 <nettima0711@gmail.com>
2026-09-13 16:36:47 -0300

    01_criar_tabelas_raw.sql

--- sql/01_criar_tabelas_raw.sql
+++ sql/01_criar_tabelas_raw.sql
@@ -1,1 +1,37 @@
+CREATE DATABASE IF NOT EXISTS atividade2;
 
+CREATE EXTERNAL TABLE atividade2.clientes (
+    cliente_id INT,
+    nome STRING,
+    uf STRING
+)
+PARTITIONED BY (ingest_date STRING)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+    'separatorChar'=','
+)
+STORED AS TEXTFILE
+LOCATION 's3://atividade2-pipeline-mariana10782261-2026/raw/clientes/';
+
+CREATE EXTERNAL TABLE atividade2.produtos (
+    product_id INT,
+    descricao STRING,
+    categoria STRING,
+    preco DOUBLE
+)
+PARTITIONED BY (ingest_date STRING)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+    'separatorChar'=','
+)
+STORED AS TEXTFILE
+LOCATION 's3://atividade2-pipeline-mariana10782261-2026/raw/produtos/';
+
+CREATE EXTERNAL TABLE atividade2.pedidos (
+    pedido_id INT,
+    cliente_id INT,
+    product_id INT,
+    quantidade INT
+)
+PARTITIONED BY (ingest_date STRING)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.
